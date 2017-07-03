efb8cd0accd92133841467ef9f4495a08ee3d58a
Paul Borawski <paulborawski14@gmail.com>
2017-07-02 22:51:08 -0700

    now changes all divs to one of six colors

--- popup.js
+++ popup.js
@@ -4,9 +4,11 @@ document.addEventListener('DOMContentLoaded', function() {
 
     //chrome.tabs.executeScript(integer tabId, object details, function callback)
     chrome.tabs.executeScript(null,{code:`
+      let randomColors = ['red', 'yellow', 'blue', 'green', 'black', 'orange']
+      var colorItem = randomColors[Math.floor(Math.random()*randomColors.length)];
       var h1s = document.querySelectorAll('div'), l = h1s.length
       while(l--)
-       h1s[l].style.color = 'red';
+       h1s[l].style.color = colorItem;
       `})
 
   }, false);
